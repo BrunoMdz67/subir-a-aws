6a874aa7f54da6e6f7b0745c6ac0fd76b0d3d4bf
BrunoMdz <brunomdz@hotmail.com>
2023-06-08 21:54:21 -0600

    listo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css'
-import ImageUploader from 'react-images-upload';
+import ImageUploader from './ImageUploader';
+import './ImageUploader.css'
 
 function App() {
 	return (
 	  <div>
+		
 		<h1>My App</h1>
 		<ImageUploader />
 	  </div>
